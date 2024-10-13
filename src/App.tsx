08b34daf60f848e35c78e7761ab0139388d9e3d7
mrdeepak125 <asus0gl8@outlook.com>
2024-10-13 13:19:25 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import PlatformDownloader from './components/PlatformDownloader'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <PlatformDownloader />
+      <Analytics />
     </div>
   )
 }
